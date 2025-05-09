ba59d91e27fb97e35aa49fa5738a5afa84c02519
Adrien <adrien.costandi@gmail.com>
2025-05-09 09:22:37 +0200

    first packets parsed by PacketReader

---
+++ src/main/java/fr/uge/net/adrien/readers/packets/PacketReader.java
@@ -0,0 +1,85 @@
+package fr.uge.net.adrien.readers.packets;
+
+import fr.uge.net.adrien.packets.Packet;
+import fr.uge.net.adrien.readers.Reader;
+import java.nio.ByteBuffer;
+
+public class PacketReader implements Reader<Packet> {
+
+  private Packet packet;
+  private final OpCodeReader opcodeReader = new OpCodeReader();
+  private State state = State.WAITING_OPCODE;
+  private Reader<? extends Packet> payloadReader;
+
+  @Override
+  public ProcessStatus process(ByteBuffer buffer) {
+    if (state == State.ERROR) {
+      throw new IllegalStateException("cannot perform process if had an error");
+    }
+    if (state == State.DONE) {
+      throw new IllegalStateException("cannot perform process if already done");
+    }
+    if (state == State.WAITING_OPCODE) {
+      var status = opcodeReader.process(buffer);
+
+      switch (status) {
+        case REFILL -> {
+          return ProcessStatus.REFILL;
+        }
+        case ERROR -> {
+          state = State.ERROR;
+          return ProcessStatus.ERROR;
+        }
+        case DONE -> {
+          var opcode = opcodeReader.get();
+          opcodeReader.reset();
+          state = State.WAITING_PAYLOAD;
+
+          payloadReader = switch (opcode) {
+            case CONNECT_NO_AUTH -> new ConnectNoAuthReader();
+            case CONNECT_AUTH -> throw new UnsupportedOperationException("NOT IMPLEMENTED YET");
+            case CONNECT_SERVER_RESPONSE -> new ConnectServerResponseReader();
+          };
+        }
+      }
+    }
+
+    var status = payloadReader.process(buffer);
+
+    switch (status) {
+      case REFILL -> {
+        return ProcessStatus.REFILL;
+      }
+      case DONE -> {
+        packet = payloadReader.get();
+        state = State.DONE;
+        payloadReader.reset();
+        return ProcessStatus.DONE;
+      }
+      default -> {
+        state = State.ERROR;
+        return ProcessStatus.ERROR;
+      }
+    }
+  }
+
+  @Override
+  public Packet get() {
+    if (state != State.DONE) {
+      throw new IllegalStateException("cannot get the value if process didn't finished");
+    }
+    return packet;
+  }
+
+  @Override
+  public void reset() {
+    opcodeReader.reset();
+    packet = null;
+    state = State.WAITING_OPCODE;
+    payloadReader = null;
+  }
+
+  private enum State {
+    DONE, WAITING_OPCODE, WAITING_PAYLOAD, ERROR
+  }
+}
---
+++ src/main/test/fr/uge/net/adrien/readers/packets/PacketReaderTest.java
@@ -0,0 +1,112 @@
+package fr.uge.net.adrien.readers.packets;
+
+import fr.uge.net.adrien.packets.ConnectNoAuth;
+import fr.uge.net.adrien.packets.ConnectServerResponse;
+import fr.uge.net.adrien.packets.Packet;
+import fr.uge.net.adrien.readers.Reader;
+import java.nio.ByteBuffer;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PacketReaderTest {
+
+  public static List<Packet> packets() {
+    return List.of(new ConnectNoAuth("Bob"),
+        // new ConnectAuth("Bob", "password1234"), TODO
+        new ConnectServerResponse(ConnectServerResponse.StatusCode.OK));
+  }
+
+  @Test
+  public void empty() {
+    var buffer = ByteBuffer.allocate(1024);
+    var reader = new PacketReader();
+
+    assertEquals(Reader.ProcessStatus.REFILL, reader.process(buffer));
+
+    // Verify buffer position and limit
+    assertEquals(0, buffer.position());
+    assertEquals(buffer.capacity(), buffer.limit());
+  }
+
+  @ParameterizedTest
+  @MethodSource("packets")
+  public void simple(Packet packet) {
+    var buffer = ByteBuffer.allocate(1024);
+    buffer.put(packet.toByteBuffer().flip());
+
+    var reader = new PacketReader();
+    assertEquals(Reader.ProcessStatus.DONE, reader.process(buffer));
+    assertEquals(packet, reader.get());
+
+    // vérifie qu'on a remis en mode ecriture (compact)
+    assertEquals(0, buffer.position());
+    assertEquals(buffer.capacity(), buffer.limit());
+  }
+
+  @ParameterizedTest
+  @MethodSource("packets")
+  void processRefill(Packet packet) {
+    var buffer = ByteBuffer.allocate(1024);
+    buffer.put(packet.toByteBuffer().flip());
+    buffer.limit(0); // not the size of a byte
+
+    var reader = new PacketReader();
+    assertEquals(Reader.ProcessStatus.REFILL, reader.process(buffer));
+
+    buffer = ByteBuffer.allocate(1024);
+    buffer.put(packet.toByteBuffer().flip());
+
+    assertEquals(Reader.ProcessStatus.DONE, reader.process(buffer));
+    assertEquals(packet, reader.get());
+  }
+
+  @ParameterizedTest
+  @MethodSource("packets")
+  void processTwice(Packet packet) {
+    var buffer = ByteBuffer.allocate(1024);
+    buffer.put(packet.toByteBuffer().flip());
+
+    var reader = new PacketReader();
+    assertEquals(Reader.ProcessStatus.DONE, reader.process(buffer));
+    assertThrows(IllegalStateException.class, () -> reader.process(buffer));
+  }
+
+  @Test
+  public void errorGet() {
+    var reader = new PacketReader();
+    assertThrows(IllegalStateException.class, () -> {
+      var res = reader.get();
+    });
+  }
+
+  @ParameterizedTest
+  @MethodSource("packets")
+  void reset(Packet packet) {
+    var buffer = ByteBuffer.allocate(1024);
+    var value2 = new ConnectNoAuth("Albert");
+    buffer.put(packet.toByteBuffer().flip());
+    buffer.put(value2.toByteBuffer().flip());
+
+    var reader = new PacketReader();
+    assertEquals(Reader.ProcessStatus.DONE, reader.process(buffer));
+    assertEquals(packet, reader.get());
+
+    // vérifie qu'on a remis en mode ecriture (compact)
+    assertEquals(9, buffer.position()); // Albert
+    assertEquals(buffer.capacity(), buffer.limit());
+
+    reader.reset();
+
+    assertEquals(Reader.ProcessStatus.DONE, reader.process(buffer));
+    assertEquals(value2, reader.get());
+
+    // vérifie qu'on a remis en mode ecriture (compact)
+    assertEquals(0, buffer.position());
+    assertEquals(buffer.capacity(), buffer.limit());
+  }
+}
