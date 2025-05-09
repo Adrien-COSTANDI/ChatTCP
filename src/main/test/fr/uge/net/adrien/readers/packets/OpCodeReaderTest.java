d824c05803568e357ff4378a3bd0b09796218727
Adrien <adrien.costandi@gmail.com>
2025-05-09 09:20:29 +0200

    added parametrized tests for OpCodeReaderTest

--- src/main/test/fr/uge/net/adrien/readers/packets/OpCodeReaderTest.java
+++ src/main/test/fr/uge/net/adrien/readers/packets/OpCodeReaderTest.java
@@ -4,9 +4,13 @@ import fr.uge.net.adrien.packets.Packet;
 import fr.uge.net.adrien.readers.Reader;
 import java.nio.ByteBuffer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class OpCodeReaderTest {
 
@@ -36,6 +40,45 @@ class OpCodeReaderTest {
     assertEquals(0, buffer.position());
     assertEquals(buffer.capacity(), buffer.limit());
   }
+
+  @ParameterizedTest
+  @EnumSource(Packet.Opcode.class)
+  public void checkOpcodeValuesAreValid(Packet.Opcode opcode) {
+    assertTrue(opcode.value() >= 1 && opcode.value() <= 11);
+  }
+
+  @Test
+  public void valuesMatch() {
+    assertAll(() -> {
+      assertEquals(1, Packet.Opcode.CONNECT_NO_AUTH.value());
+      assertEquals(2, Packet.Opcode.CONNECT_AUTH.value());
+      assertEquals(3, Packet.Opcode.CONNECT_SERVER_RESPONSE.value());
+      // assertEquals(4, Packet.Opcode.CLIENT_PUBLIC_MESSAGE.value());
+      // assertEquals(5, Packet.Opcode.SERVER_FORWARD_PUBLIC_MESSAGE.value());
+      // assertEquals(6, Packet.Opcode.DM_REQUEST.value());
+      // assertEquals(7, Packet.Opcode.DM_RESPONSE.value());
+      // assertEquals(8, Packet.Opcode.DM_CONNECT.value());
+      // assertEquals(9, Packet.Opcode.DM_TEXT.value());
+      // assertEquals(10, Packet.Opcode.DM_FILE_HEADER.value());
+      // assertEquals(11, Packet.Opcode.DM_FILE_CONTENT.value());
+    });
+  }
+
+  @ParameterizedTest
+  @EnumSource(Packet.Opcode.class)
+  public void everyOpcode(Packet.Opcode opcode) {
+    var buffer = ByteBuffer.allocate(1024);
+    buffer.put(opcode.value());
+
+    var reader = new OpCodeReader();
+    assertEquals(Reader.ProcessStatus.DONE, reader.process(buffer));
+    assertEquals(opcode, reader.get());
+
+    // vérifie qu'on a remis en mode ecriture (compact)
+    assertEquals(0, buffer.position());
+    assertEquals(buffer.capacity(), buffer.limit());
+  }
+
 
   @Test
   void processRefill() {
